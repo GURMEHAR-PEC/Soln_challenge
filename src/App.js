e566dea76f408f9050661e9133984ee74c8ba363
firepixie18 <118536106+firepixie18@users.noreply.github.com>
2024-03-28 12:33:33 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,15 +2,19 @@ import React from 'react';
 import SignInSignUp from '../src/components/SignInSignUp';
 import './App.css';
 import { Map } from './components/Map';
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route, Form } from "react-router-dom";
+import IncidentReportForm from './components/form';
 
 function App() {
   return (
     <div className="App">
+      
+      
       <Router>
         <Routes>
           <Route path="/" element={<SignInSignUp />} />
           <Route path="/map" element={<Map />} />
+          <Route path="/feedback" element={<IncidentReportForm />} />
         </Routes>
       </Router>
     </div>
